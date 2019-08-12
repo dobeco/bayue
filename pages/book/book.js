1c776b3ea66c0221c2048b88eda71b7ae27b8ea2
dobeco <736278325@qq.com>
2019-08-12 20:31:29 +0800

    搜索组件

--- pages/book/book.js
+++ pages/book/book.js
@@ -7,7 +7,9 @@ Page({
    * 页面的初始数据
    */
   data: {
-    books: []
+    books: [],
+    searchPanel: false,
+    more: false
 
   },
 
@@ -43,52 +45,30 @@ Page({
 
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
+
   onReady: function() {
 
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function() {
-
+  onActivateSearch: function (event) {
+    this.setData({
+      searchPanel: true
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function() {
-
+  onCancel: function (event) {
+    this.setData({
+      searchPanel: false
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function() {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function() {
+  onShareAppMessage() {
 
   }
+
+  
+
+ 
+
+
 })
