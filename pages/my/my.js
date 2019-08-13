e2e178d3e9e45a9b11c3f2e1d3417109ec9fe8ad
dobeco <736278325@qq.com>
2019-08-13 18:24:30 +0800

    自定义button实现获取用户信息

--- pages/my/my.js
+++ pages/my/my.js
@@ -1,4 +1,9 @@
-// pages/my/my.js
+import ClassicModel from '../../models/classic.js'
+import  BookModel from '../../models/book.js'
+
+let classicModel = new ClassicModel()
+let bookModel = new BookModel()
+
 Page({
 
   /**
@@ -8,59 +13,9 @@ Page({
 
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  onGetUserInfo: function(event) {
+    let userInfo = event.detail.userInfo
+    console.log(userInfo)
+  
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })
